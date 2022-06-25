90baeb5e0726eb22724ef0e29a4d74c7b14ccb46
ma-lacroix <lacroix.marcantoine@gmail.com>
2022-06-25 22:37:51 +0200

    Bank card classes

---
+++ src/bc.hpp
@@ -0,0 +1,19 @@
+#ifndef BC_hpp
+#define BC_hpp
+#include <string>
+#include "card.hpp"
+
+class bankCard: public card {
+private:
+    std::string bc_number;
+    bool status;    
+public:
+    bankCard(std::string);
+    ~bankCard();
+    void checkStatus();
+    bool getStatus();
+    std::vector<int> changeToInt();
+    std::string getNumber();
+};
+
+#endif // BC_hpp
